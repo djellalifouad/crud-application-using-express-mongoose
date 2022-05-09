3856f707f95a84f99dd718b7ef5792c104f49d34
fouadesi <83358349+djellalifouad@users.noreply.github.com>
2022-05-09 11:24:30 +0100

    update

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const db = require("../models");
+const { route } = require('./users');
 const User = db.users;
 
 /* GET home page. */
@@ -12,5 +13,31 @@ router.get('/', function(req, res, next) {
     })
 
 });
+router.delete('/delete/:id',(req,res)=> {
+  console.log('here');
+   User.deleteOne({id:req.params.id})
+    .then(data => {
+      console.log(data);
+        res.json(data)
+    })
+})
+router.put('/put/:username',(req,res)=> {
+  console.log('here');
+   User.updateOne({username:req.params.username},{password: req.body.password})
+    .then(data => {
+      console.log(data);
+        res.json(data)
+    })
+})
+router.get('/getuseerjosn', function(req, res, next) {
+   User.find()
+    .then(data => {
+      console.log(data);
+        res.json(data)
+    })
+});
+
+
+
 
 module.exports = router;
